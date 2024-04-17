3c1718dd0b50994ed831a9ef195e2220470249f1
unknown <hardeyshinnah.mo@gmail.com>
2024-04-17 12:33:18 +0100

    about and contact

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image"
+import profileCharacter from "../../../public/image/character.png"
+import ContactForm from "./ContactForm"
+
+const Contact = () => {
+  return (
+    <section className="w-full h-auto md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex  flex-col md:flex-row items-center justify-center text-dark dark:text-light">
+        <div className="inline-block w-full sm:w-4/5 md:w-2/5 h-full md:border-r-2 border-solid border-dark dark:border-light">
+        <div className='w-full md:w-1/2 h-full border-r-2 border-solid border-dark dark:border-light flex justify-center'> 
+            <Image src={profileCharacter} alt="CodeBucks" 
+                className='w-4/5  xs:w-3/4 md:w-full h-full object-contain object-center'
+                priority
+                sizes="(max-width: 768px) 100vw,(max-width: 1180px) 50vw, 50vw"
+            />
+        </div>
+        </div>
+            <div className="w-full  md:w-3/5 flex flex-col items-start justify-center px-5 xs:px-10 md:px-16 pb-8">
+            <h2 className="font-bold capitalize  text-2xl xs:text-3xl sm:text-4xl">Let's Connect!</h2>
+            <ContactForm />
+            </div>
+  </section>
+  )
+}
+
+export default Contact
