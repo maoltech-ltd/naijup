75c1816da4b26479706ab5f8d931cacdede01209
unknown <hardeyshinnah.mo@gmail.com>
2024-04-23 22:31:50 +0100

    signin

---
+++ src/app/(account)/signin.tsx/page.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import * as Yup from 'yup';
+
+const Login = () => {
+    const router = useRouter();
+
+    // form validation rules 
+    const validationSchema = Yup.object().shape({
+        username: Yup.string().required('Username is required'),
+        email: Yup.string().email().required('Email is required'),
+        password: Yup.string().required('Password is required')
+    });
+     const formOptions = validationSchema.validate();
+
+    // get functions to build form with useForm() hook
+    const { register, handleSubmit, formState } = useForm(formOptions);
+    const { errors } = formState;
+
+    function onSubmit(data: any) {
+        console.log(data);
+    }
+
+    return (
+        <div>
+            <div className="card">
+                <h4 className="card-header">Login</h4>
+                <div className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input name="username" type="text" {...register('username')} className={`form-control ${errors.username ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.username?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input name="email" type="text" {...register('email')} className={`form-control ${errors.email ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.email?.message}</div>
+                        </div>
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input name="password" type="password" {...register('password')} className={`form-control ${errors.password ? 'is-invalid' : ''}`} />
+                            <div className="invalid-feedback">{errors.password?.message}</div>
+                        </div>
+                        <button disabled={formState.isSubmitting} className="btn btn-primary">
+                            {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                            Login
+                        </button>
+                        <Link href="/register" className="btn btn-link">Register</Link>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
