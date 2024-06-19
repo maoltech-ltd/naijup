fb329b5861c7fe8302f0676f8506ed3a0200b902
mujeeb olagunju <hardeyshinnah.mo@gmail.com>
2024-06-19 16:11:37 -0700

    mutate

--- src/components/Profile/ProfileDetails.tsx
+++ src/components/Profile/ProfileDetails.tsx
@@ -1,57 +1,26 @@
 "use client";
-
 import { Avatar, Button, useDisclosure } from "@nextui-org/react";
 import React from "react";
-
 import AuthModal from "../Auth/AuthModal";
-
-// import { useAppDispatch, useAppSelector } from "@/hooks/reduxHooks";
-// import { setMoreInfo } from "@/redux/userSlice";
-
-import toast from "react-hot-toast";
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
-import axios from "axios";
-// import moment from "moment";
+import moment from "moment";
 import Link from "next/link";
 import { BackIcon } from "../icon";
+import { useAppDispatch } from "@/src/redux/hooks/dispatch";
+import { useSelector } from "react-redux";
+//import mutate from "swr";
 
 const ProfileDetails = ({ user }: any) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-//   const dispatch = useAppDispatch();
-
-//   const currentUser = useAppSelector((state) => state.auth);
-//   const { moreInfo } = useAppSelector((state) => state.user);
-
-//   const queryClient = useQueryClient();
-
-//   const { mutate } = useMutation({
-//     mutationFn: async (userData: { userId: string }) => {
-//       try {
-//         const { data } = await axios.post("/api/users/follow", {
-//           userId: userData.userId,
-//         });
-//         console.log(data);
-//         return data;
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     },
-//     onSuccess: (data: any) => {
-//       toast.success(data.message);
-//     //   queryClient.invalidateQueries(["me"]);
-//     },
-//     onError: (error: any) => {
-//       toast.error(error.message);
-//     },
-//   });
+   const dispatch = useAppDispatch();
+   const currentUser = useSelector((state: any) => state.user);
 
   const handleFollow = () => {
-    // if (currentUser.authStatus) {
-    //   mutate({ userId: user.id });
-    // } else {
-    //   onOpen();
-    // }
+    if (currentUser.authStatus) {
+       mutate({ userId: user.id });
+     } else {
+       onOpen();
+     }
   };
 
   return (
@@ -64,9 +33,7 @@ const ProfileDetails = ({ user }: any) => {
             name="username"
           />
           <div className="flex flex-col justify-end gap-4">
-            {
-            /* {currentUser.user?.id === user.id */
-            true
+             {currentUser.user?.id === user.id
              ? (
               <Button color="primary" radius="sm" as={Link} href="/setting">
                 Edit Profile
@@ -74,11 +41,10 @@ const ProfileDetails = ({ user }: any) => {
             ) : (
               <div className="flex items-center gap-4">
                 <Button color="primary" radius="sm" onClick={handleFollow}>
-                  {
-                //   currentUser.user?.followingIDs.includes(user.id)
+                   currentUser.user?.followingIDs.includes(user.id)
                     true
                     ? "unFollow"
-                    : "Follow"}
+                    : "Follow"
                 </Button>
                 <Button isIconOnly variant="light">
                     //more icon
@@ -90,23 +56,23 @@ const ProfileDetails = ({ user }: any) => {
         </div>
         <div className="pt-4">
           <h1 className="lg:text-2xl md:text-xl text-lg md:font-bold font-medium">
-            {/*user.name*/}
-            username
+            user.name
           </h1>
-          <p>@username
-            {/*user.username*/}</p>
-          <p className="text-lg py-2">lorem ipsume ...{/*user.bio*/}</p>
+          <p>
+            user.username
+          </p>
+          <p className="text-lg py-2">user.bio</p>
           <div className="flex gap-4 py-2">
             <div className="flex gap-1">
               <p className="font-semibold text-default-500 text-small">
-                {/*user.followingIDs.length*/}
+                user.followingIDs.length
                 4
               </p>
               <p className=" text-default-500 text-small">Following</p>
             </div>
             <div className="flex gap-1">
               <p className="font-semibold text-default-500 text-small">
-                {/*user.followerIDs.length*/}
+                user.followerIDs.length
                 6
               </p>
               <p className="text-default-500 text-small">Followers</p>
@@ -116,8 +82,7 @@ const ProfileDetails = ({ user }: any) => {
             
             <BackIcon name="cake" />
             <span>
-              {/* joined on {moment(user.createdAt, moment.ISO_8601).format("l")} */}
-              moment details
+               joined on {moment(user.createdAt, moment.ISO_8601).format("l")}
             </span>
           </div>
         </div>
@@ -131,7 +96,7 @@ const ProfileDetails = ({ user }: any) => {
             className="font-semibold mt-4 md:hidden text-neutral-600"
             // onClick={() => dispatch(setMoreInfo(true))}
           >
-            More info about @username{/*user.username*/}
+            user.username
           </Button>
         {/* )} */}
       </div>
