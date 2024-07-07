f05e3e13d24be05df816fdc3b87cf8c887d522d5
mujeeb olagunju <hardeyshinnah.mo@gmail.com>
2024-07-07 00:56:31 -0700

    status modal and categories

--- src/app/categories/[slug]/page.tsx
+++ src/app/categories/[slug]/page.tsx
@@ -1,35 +1,117 @@
+// "use client";
+// import { blogDetailsProp } from "@/src/utils/props"
+// import { blogHome } from "@/src/content"
+// import { sortBlogs } from "@/src/utils"
+// import Categories from "@/src/components/Blog/Categories";
+// import BlogLayoutThree from "@/src/components/Blog/BlogLayoutThree";
+// import { useAppDispatch } from "@/src/redux/hooks/dispatch";
+// import { useEffect } from "react";
+// import { fetchCategories } from "@/src/redux/slice/categorySlice";
+// import { useSelector } from "react-redux";
+
+
+// // export async function generateStaticParams() {
+// //    const categories: string[] = [];
+// //    const paths = [{slug: "all"}]
+// //    blogHome.map(blog =>{
+// //     if(blog.isPublished){
+// //         if(!categories.includes(blog.category))
+// //         categories.push(blog.category)
+// //         paths.push({slug: blog.category})
+// //     }
+// //    })
+// //     return paths;
+// // }
+
+// const CategoryPage = async({ params }: { params: { slug: string } }) => {
+
+//   const dispatch = useAppDispatch();
+//   await dispatch(fetchCategories(params.slug));
+//   const data = useSelector((state: any) => state.categories);
+//   console.log({data})
+//   const { categories, status, error } = data;
+
+//   if (status === 'loading') {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (status === 'failed') {
+//     return <div>Error: {error}</div>;
+//   }
+//   if(status === 'succeeded'){
+  
+//   const blogs = categories;
+  
+  
+//   const sortedBlogs = sortBlogs(blogs)
+
+//   const allCategories: string[] = [];
+
+//   sortedBlogs.forEach((blog: any) => {
+//       if (!allCategories.includes(blog.category)) {
+//           allCategories.push(blog.category);
+//       }
+//   });
+
+//     return (
+//       <article className="mt-12 flex flex-col text-dark dark:text-light">
+//         <div className=" px-5 sm:px-10  md:px-24  sxl:px-32 flex flex-col">
+//           <h1 className="mt-6 font-semibold text-2xl md:text-4xl lg:text-5xl">#{params.slug}</h1>
+//           <span className="mt-2 inline-block">
+//             Discover more categories and expand your knowledge!
+//           </span>
+//         </div>
+//         <Categories categories={allCategories} currentSlug={params.slug} />
+
+//         <div className="grid  grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 grid-rows-2 gap-16 mt-5 sm:mt-10 md:mt-24 sxl:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32">
+//           {blogs.map((blog: any, index: number) => (
+//             <article key={index} className="col-span-1 row-span-1 relative">
+//               <BlogLayoutThree blog={blog} />
+//             </article>
+//           ))}
+//         </div>
+//       </article>
+//     )
+//   }
+// }
+
+// export default CategoryPage
+
 "use client";
-import { blogDetailsProp } from "@/src/utils/props"
-import { blogHome } from "@/src/content"
-import { sortBlogs } from "@/src/utils"
+import React, { useEffect, useState } from "react";
+import { sortBlogs } from "@/src/utils";
 import Categories from "@/src/components/Blog/Categories";
 import BlogLayoutThree from "@/src/components/Blog/BlogLayoutThree";
 import { useAppDispatch } from "@/src/redux/hooks/dispatch";
-import { useEffect } from "react";
 import { fetchCategories } from "@/src/redux/slice/categorySlice";
 import { useSelector } from "react-redux";
-
-
-// export async function generateStaticParams() {
-//    const categories: string[] = [];
-//    const paths = [{slug: "all"}]
-//    blogHome.map(blog =>{
-//     if(blog.isPublished){
-//         if(!categories.includes(blog.category))
-//         categories.push(blog.category)
-//         paths.push({slug: blog.category})
-//     }
-//    })
-//     return paths;
-// }
+import SuccessModal from "@/src/components/Modal/SuccessModal";
+import ErrorModal from "@/src/components/Modal/ErrorModal";
 
 const CategoryPage = async({ params }: { params: { slug: string } }) => {
-
   const dispatch = useAppDispatch();
-  await dispatch(fetchCategories(params.slug));
+
+  const [isErrorOpen, setIsErrorOpen] = useState(false);
+  const [isSuccessOpen, setIsSuccessOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+  useEffect(() => {
+    dispatch(fetchCategories(params.slug));
+  }, [dispatch, params.slug]);
+
   const data = useSelector((state: any) => state.categories);
-  console.log({data})
   const { categories, status, error } = data;
+
+  useEffect(() => {
+    if (status === 'failed') {
+      setErrorMessage(error);
+      setIsErrorOpen(true);
+    } else if (status === 'succeeded') {
+      setSuccessMessage("Categories fetched successfully!");
+      setIsSuccessOpen(true);
+    }
+  }, [status, error]);
 
   if (status === 'loading') {
     return <div>Loading...</div>;
@@ -38,41 +120,42 @@ const CategoryPage = async({ params }: { params: { slug: string } }) => {
   if (status === 'failed') {
     return <div>Error: {error}</div>;
   }
-  if(status === 'succeeded'){
-  
-  const blogs = categories;
-  
-  
-  const sortedBlogs = sortBlogs(blogs)
 
-  const allCategories: string[] = [];
+  if (status === 'succeeded') {
+    const blogs = categories;
+    const sortedBlogs = sortBlogs(blogs);
 
-  sortedBlogs.forEach((blog: any) => {
+    const allCategories: string[] = [];
+    sortedBlogs.forEach((blog: any) => {
       if (!allCategories.includes(blog.category)) {
-          allCategories.push(blog.category);
+        allCategories.push(blog.category);
       }
-  });
+    });
 
     return (
-      <article className="mt-12 flex flex-col text-dark dark:text-light">
-        <div className=" px-5 sm:px-10  md:px-24  sxl:px-32 flex flex-col">
+      <main className="mt-12 flex flex-col text-dark dark:text-light">
+        <div className="px-5 sm:px-10 md:px-24 sxl:px-32 flex flex-col">
           <h1 className="mt-6 font-semibold text-2xl md:text-4xl lg:text-5xl">#{params.slug}</h1>
           <span className="mt-2 inline-block">
             Discover more categories and expand your knowledge!
           </span>
         </div>
         <Categories categories={allCategories} currentSlug={params.slug} />
-
-        <div className="grid  grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 grid-rows-2 gap-16 mt-5 sm:mt-10 md:mt-24 sxl:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-16 mt-5 sm:mt-10 md:mt-24 sxl:mt-32 px-5 sm:px-10 md:px-24 sxl:px-32">
           {blogs.map((blog: any, index: number) => (
             <article key={index} className="col-span-1 row-span-1 relative">
               <BlogLayoutThree blog={blog} />
             </article>
           ))}
         </div>
-      </article>
-    )
-  }
-}
 
-export default CategoryPage
+        <ErrorModal isOpen={isErrorOpen} onClose={() => setIsErrorOpen(false)} message={errorMessage} />
+        <SuccessModal isOpen={isSuccessOpen} onClose={() => setIsSuccessOpen(false)} message={successMessage} />
+      </main>
+    );
+  }
+
+  return null;
+};
+
+export default CategoryPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,102 @@
+// "use client";
+// import HomeCoverSection from "../components/Home/HomeCoverSection";
+// import FeaturedPost from "../components/Home/FeaturedPost";
+// import RecentPost from "../components/Home/RecentPost";
+// import { useEffect } from "react";
+// import { useSelector } from "react-redux";
+// import { useAppDispatch } from "../redux/hooks/dispatch";
+// import { fetchPosts } from "../redux/slice/postSlice";
+// //import dynamic from 'next/dynamic';
+// export default function Home() {
+  
+//   // const HomeCoverSection = dynamic(() => import("../components/Home/HomeCoverSection"));
+//   // const FeaturedPost = dynamic(() => import("../components/Home/FeaturedPost"));
+//   // const RecentPost = dynamic(() => import("../components/Home/RecentPost"));
+  
+//   const dispatch = useAppDispatch();
+  
+//   useEffect(() => {
+//     dispatch(fetchPosts());
+//   }, []);
+
+//   const post = useSelector((state: any) => state.post
+//   ); 
+//   const {posts, status, error} = post
+
+//   if (status === 'loading') {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (status === 'failed') {
+//     return <div>Error: {error}</div>;
+//   }
+//   if(status === 'succeeded'){
+  
+//   return (
+//     <main className="flex flex-col items-center justify-center">
+//       <HomeCoverSection blogs={posts.results[0]} />
+//       <FeaturedPost blogs={posts.results} />
+//       <RecentPost blogs={posts} />
+//     </main>
+//   );
+// }
+
+// }
+
 "use client";
-import HomeCoverSection from "../components/Home/HomeCoverSection";
-import FeaturedPost from "../components/Home/FeaturedPost";
-import RecentPost from "../components/Home/RecentPost";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useAppDispatch } from "../redux/hooks/dispatch";
 import { fetchPosts } from "../redux/slice/postSlice";
-//import dynamic from 'next/dynamic';
-export default function Home() {
-  
-  // const HomeCoverSection = dynamic(() => import("../components/Home/HomeCoverSection"));
-  // const FeaturedPost = dynamic(() => import("../components/Home/FeaturedPost"));
-  // const RecentPost = dynamic(() => import("../components/Home/RecentPost"));
-  
+import HomeCoverSection from "../components/Home/HomeCoverSection";
+import FeaturedPost from "../components/Home/FeaturedPost";
+import RecentPost from "../components/Home/RecentPost";
+import ErrorModal from "../components/Modal/ErrorModal";
+import SuccessModal from "../components/Modal/SuccessModal";
+
+const Home: React.FC = () => {
   const dispatch = useAppDispatch();
   
+  const [isErrorOpen, setIsErrorOpen] = useState(false);
+  const [isSuccessOpen, setIsSuccessOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
   useEffect(() => {
     dispatch(fetchPosts());
-  }, []);
+  }, [dispatch]);
 
-  const post = useSelector((state: any) => state.post
-  ); 
-  const {posts, status, error} = post
+  const post = useSelector((state: any) => state.post); 
+  const { posts, status, error } = post;
+
+  useEffect(() => {
+    if (status === 'failed') {
+      setErrorMessage(error);
+      setIsErrorOpen(true);
+    } else if (status === 'succeeded') {
+      setSuccessMessage("Posts fetched successfully!");
+      setIsSuccessOpen(true);
+    }
+  }, [status, error]);
 
   if (status === 'loading') {
     return <div>Loading...</div>;
   }
 
-  if (status === 'failed') {
-    return <div>Error: {error}</div>;
-  }
-  if(status === 'succeeded'){
-  
   return (
     <main className="flex flex-col items-center justify-center">
-      <HomeCoverSection blogs={posts.results[0]} />
-      <FeaturedPost blogs={posts.results} />
-      <RecentPost blogs={posts} />
+      {status === 'succeeded' && (
+        <>
+          <HomeCoverSection blogs={posts.results[0]} />
+          <FeaturedPost blogs={posts.results} />
+          <RecentPost blogs={posts} />
+        </>
+      )}
+      <ErrorModal isOpen={isErrorOpen} onClose={() => setIsErrorOpen(false)} message={errorMessage} />
+      <SuccessModal isOpen={isSuccessOpen} onClose={() => setIsSuccessOpen(false)} message={successMessage} />
     </main>
   );
-}
+};
 
-}
+export default Home;
+
