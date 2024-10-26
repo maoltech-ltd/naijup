996698e7a323950915cdc7e63326a782af950712
mujeeb olagunju <hardeyshinnah.mo@gmail.com>
2024-10-26 13:29:46 +0100

    blog set up

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,14 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/**',
-      }],
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
   }
 };
 export default nextConfig;
--- src/app/createpost/page.tsx
+++ src/app/createpost/page.tsx
@@ -1,20 +1,38 @@
-"use client";
-import Editor from '@/src/components/Post/Editor'
-import ProtectedRoute from '@/src/components/ProtectedRoute'
-import React from 'react'
+// "use client";
+// import Editor from '@/src/components/Post/Editor'
+// import ProtectedRoute from '@/src/components/ProtectedRoute'
+// import React from 'react'
 
-const createPost = () => {
+// const createPost = () => {
+//   return (
+//     <ProtectedRoute>
+
+//       {({ user }: any) => (
+//         <div>
+//           <Editor post={null} user={user} />
+//         </div>
+//       )}
+
+//     </ProtectedRoute>
+//   )
+// }
+
+// export default createPost
+"use client";
+import Editor from '@/src/components/Post/Editor';
+import ProtectedRoute from '@/src/components/ProtectedRoute';
+import React from 'react';
+
+const CreatePost = () => {
   return (
     <ProtectedRoute>
-
       {({ user }: any) => (
-        <div>
+        <div className="flex justify-center items-center py-6 px-4 sm:px-6 lg:px-8">
           <Editor post={null} user={user} />
         </div>
       )}
-
     </ProtectedRoute>
-  )
-}
+  );
+};
 
-export default createPost
+export default CreatePost;
