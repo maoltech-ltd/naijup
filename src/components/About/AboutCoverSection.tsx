3aaf58fb0b5e7e831ef347ba02db7006fba27230
maoltech <hardeyshinnah.mo@gmail.com>
2025-10-03 20:30:04 +0100

    about redifine

--- src/components/About/AboutCoverSection.tsx
+++ src/components/About/AboutCoverSection.tsx
@@ -1,31 +1,3 @@
-// "use client";
-// import Image from "next/image"
-// import profileCharacter from "@/public/image/character.png"
-
-// const AboutCoverSection = () => {
-//   return (
-//     <section className='w-full md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light'>
-//         <div className='w-full md:w-1/2 h-full border-r-2 border-solid border-dark dark:border-light flex justify-center'> 
-//             <Image src={profileCharacter} alt="CodeBucks" 
-//             className='w-4/5  xs:w-3/4 md:w-full h-full object-contain object-center'
-//             priority
-//             sizes="(max-width: 768px) 100vw,(max-width: 1180px) 50vw, 50vw"
-//             />
-//         </div>
-
-//         <div className='w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16'>
-//             <h2 className='font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left'>
-//             Dream Big, Work Hard, Achieve More!
-//             </h2>
-//             <p className='font-medium capitalize mt-4 text-base'>
-//             This Mantra Drives My Work As A Passionate Freelancer. I Blend Innovative Technology With Timeless Design For Captivating Digital Experiences. Inspired By Nature And Literature, I&apos;m A Perpetual Learner Embracing Challenges. With Each Project, I Aim To Leave A Lasting Impact—One Pixel At A Time.
-//             </p>
-//         </div>
-//     </section>
-//   )
-// }
-
-// export default AboutCoverSection
 "use client";
 import Image from "next/image"
 import profileCharacter from "@/public/image/character.png"
@@ -43,12 +15,10 @@ const AboutCoverSection = () => {
 
         <div className='w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16'>
             <h2 className='font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl text-center lg:text-left'>
-              Empowering Ideas, Inspiring Growth
+            Empowering Voices, Driving Africa’s Future
             </h2>
             <p className='font-medium capitalize mt-4 text-base'>
-              At Naijup, we tell stories that matter—covering finance, technology, entrepreneurship, startups, business, and economics. 
-              Our mission is to spotlight innovation in Nigeria and unlock opportunities across Africa. 
-              Through thoughtful insights and bold perspectives, we aim to inform, inspire, and shape the future of enterprise on the continent.
+            Naijup is more than a platform—it’s a movement to redefine Africa’s narrative. We cover finance, technology, entrepreneurship, business, and economics with clarity and depth, highlighting the people and ideas shaping tomorrow. Our mission is to connect innovation with opportunity, inspire bold thinking, and amplify stories that drive growth across Nigeria and beyond. At Naijup, we believe every insight can spark change, and every story can shape the future.
             </p>
         </div>
     </section>
